bd84642e8b985290b56573a4615e5e0ae3f568fe
adnan.drina <adnan.drina@gmail.com>
2022-02-14 15:02:09 +0100

    update

--- src/main/java/com/redhat/coolstore/InventoryResource.java
+++ src/main/java/com/redhat/coolstore/InventoryResource.java
@@ -1,29 +1,47 @@
 package com.redhat.coolstore;
 
-import org.jboss.resteasy.annotations.jaxrs.PathParam;
-
-import javax.json.Json;
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.ExceptionMapper;
-import javax.ws.rs.ext.Provider;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.json.Json;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import java.util.concurrent.TimeUnit;
+import io.micrometer.core.instrument.MeterRegistry;
 
 @Path("/services/inventory")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class InventoryResource {
 
+    private final MeterRegistry registry;
+
+    InventoryResource(MeterRegistry registry) {
+        this.registry = registry;
+    }
+
     @GET
     public List<Inventory> getAll() {
+        registry.counter("inventory.performedChecksAll.counter").increment();
+        registry.timer("inventory.performedChecksAll.timer").record(3000, TimeUnit.MILLISECONDS);
         return Inventory.listAll();
     }
 
     @GET
-    @Path("/{itemId}")
+    @Path("{itemId}")
     public List<Inventory> getAvailability(@PathParam String itemId) {
+        registry.counter("inventory.performedChecksAvail.counter").increment();
+        registry.timer("inventory.checksTimerAvail.timer").record(3000, TimeUnit.MILLISECONDS);
         return Inventory.<Inventory>streamAll()
                 .filter(p -> p.itemId.equals(itemId))
                 .collect(Collectors.toList());
